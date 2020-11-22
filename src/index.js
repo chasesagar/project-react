40bfe306a4fa05cc044d58c09ec438a4672b4e72
chasesagar <chasesagar@gmail.com>
2020-11-22 16:50:23 +0530

    First commit
    Learned about react scaffolded structure, and how to manipulate dom using class based components.
    digital signature- chasesagar-desktop

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'; // adding bootstrap into project 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
